cb86c402ab2f2b21b8d2030a3a0799a686163faf
nikisha-script <nikisha_script@mail.ru>
2022-08-26 12:51:03 +0300

    1. Liquibase. Установка. [#504892]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,6 +1,6 @@
 create table if not exists items (
-    id serial primary key,
-    name text,
-    created timestamp,
-    description text
+   id serial primary key,
+   name text,
+   created timestamp,
+   description text
 );
