1c7d1137fe5502e2cff3cc73350470f501bb5a4a
nikisha-script <nikisha_script@mail.ru>
2022-09-08 19:57:39 +0300

    1. Интеграционное тестирование с Hibernate [#6875]

--- db/scripts/tracker_sciprts.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
-
---changeset nikishin:insert_items
-INSERT INTO items (name, created) VALUES ('Item 1', '2022-08-24T15:59:57');
-INSERT INTO items (name, created) VALUES ('Item 2', '2023-08-24T15:59:57');
-INSERT INTO items (name, created) VALUES ('Item 3', '2024-08-24T15:59:57');
